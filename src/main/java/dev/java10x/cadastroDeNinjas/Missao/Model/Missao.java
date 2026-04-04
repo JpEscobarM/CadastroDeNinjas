ba2ffc75393b4b89149f27d0d84c8d7aa9d9c83f
JpEscobarM <jp.escobarmartins@gmail.com>
2026-04-04 14:31:00 -0300

    feat: add Lombok dependency and annotations to Ninja and Missao classes

--- src/main/java/dev/java10x/cadastroDeNinjas/Missao/Model/Missao.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missao/Model/Missao.java
@@ -3,20 +3,21 @@ package dev.java10x.cadastroDeNinjas.Missao.Model;
 import dev.java10x.cadastroDeNinjas.Ninjas.Enums.Ranking;
 import dev.java10x.cadastroDeNinjas.Ninjas.Model.Ninja;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "missao")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Missao {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String nome;
-
-
     private Ranking dificuldade;
 
     @OneToMany(mappedBy = "missao")
--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Model/Ninja.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Model/Ninja.java
@@ -3,11 +3,15 @@ package dev.java10x.cadastroDeNinjas.Ninjas.Model;
 
 import dev.java10x.cadastroDeNinjas.Missao.Model.Missao;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "ninja")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Ninja {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,53 +25,4 @@ public class Ninja {
     @JoinColumn(columnDefinition = "id_missao")
     private Missao missao;
 
-    public Ninja() {
-    }
-
-    public Ninja(String nome, String email, Long idade, String aldeia) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.aldeia = aldeia;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public Long getIdade() {
-        return idade;
-    }
-
-    public String getAldeia() {
-        return aldeia;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setIdade(Long idade) {
-        this.idade = idade;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setAldeia(String aldeia) {
-        this.aldeia = aldeia;
-    }
 }
